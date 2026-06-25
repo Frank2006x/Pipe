818b6b595fe9d00c83ddeee40d8e50dfe1e6682d
Frank2006x <frank2006x@gmail.com>
2026-06-25 23:45:22 +0530

    fix: raise when viper read fails

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -3,11 +3,11 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	POSTGRES_DB string `mapstructure:"POSTGRES_DB"`
-	GITHUB_CLIENT_ID string `mapstructure:"GITHUB_CLIENT_ID"`
+	POSTGRES_DB          string `mapstructure:"POSTGRES_DB"`
+	GITHUB_CLIENT_ID     string `mapstructure:"GITHUB_CLIENT_ID"`
 	GITHUB_CLIENT_SECRET string `mapstructure:"GITHUB_CLIENT_SECRET"`
-	GITHUB_CALLBACK_URL string `mapstructure:"GITHUB_CALLBACK_URL"`
-	JWT_SECRET string `mapstructure:"JWT_SECRET"`
+	GITHUB_CALLBACK_URL  string `mapstructure:"GITHUB_CALLBACK_URL"`
+	JWT_SECRET           string `mapstructure:"JWT_SECRET"`
 }
 
 func LoadConfig(path string) (Config, error) {
@@ -18,7 +18,9 @@ func LoadConfig(path string) (Config, error) {
 	viper.AutomaticEnv()
 
 	err := viper.ReadInConfig()
-
+	if err != nil {
+		return Config{}, err
+	}
 	var config Config
 
 	err = viper.Unmarshal(&config)
