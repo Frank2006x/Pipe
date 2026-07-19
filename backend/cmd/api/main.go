860debad62ea7d85b9d3e68def483bc809201388
Frank2006x <frank2006x@gmail.com>
2026-07-19 12:38:05 +0530

    feat : cors config

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -19,6 +19,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/gofiber/fiber/v3/middleware/logger"
 )
 
@@ -40,6 +41,12 @@ func main() {
 	jwtMaker := auth.NewJwtMaker(cfg.JWT_SECRET)
 	githubService := service.NewGithubService(githubClient, queries, &cfg)
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowMethods:     []string{"GET", "POST", "HEAD", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowCredentials: true,
+	}))
 	app.Use(logger.New())
 	app.Use(middleware.RequestIDMiddleware())
 
