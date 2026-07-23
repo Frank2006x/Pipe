616405a605b1b2a805c74e3558c05d8bef822765
Frank2006x <frank2006x@gmail.com>
2026-07-23 19:30:37 +0530

    feat: implement RabbitMQ producer for pipeline task queueing

--- backend/internal/queue/rabbitmq.go
+++ backend/internal/queue/rabbitmq.go
@@ -1,6 +1,10 @@
 package queue
 
 import (
+	"context"
+	"encoding/json"
+	"log"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -37,4 +41,42 @@ func NewRabbitmq(url string) (*Rabbitmq, error) {
 		channel: channel,
 		queue:   &queue,
 	}, nil
+}
+
+func (r *Rabbitmq) PublishPipeline(ctx context.Context, pipelineId int64) error {
+	message := PipelineMessage{
+		PipelineId: pipelineId,
+	}
+
+	body, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	err = r.channel.PublishWithContext(ctx,
+		"",
+		r.queue.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:  "application/json",
+			Body:         body,
+			DeliveryMode: amqp.Persistent,
+			Priority:     0,
+		})
+	if err != nil {
+		return err
+	}
+	log.Printf("Pipeline %d published successfully", pipelineId)
+	return nil
+}
+
+func (r *Rabbitmq) Close() error {
+	if r.channel != nil {
+		r.channel.Close()
+	}
+	if r.conn != nil {
+		r.conn.Close()
+	}
+	return nil
 }
